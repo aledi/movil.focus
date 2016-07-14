cd0726fbf097dd4377853adfb17ae98a8cb1ca55
Eduardo Cristerna <eduardo@schoolhub.com>
2016-07-14 18:51:04 -0500

    remove files

--- DerivedData/Focus/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-Focus.build/DerivedSources/Pods_Focus_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char Pods_FocusVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Focus  PROJECT:Pods-1" "\n"; const double Pods_FocusVersionNumber __attribute__ ((used)) = (double)1.;
