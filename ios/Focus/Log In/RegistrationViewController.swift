0f059b9652cb73fdd70c70b0bfd65a1ec5260598
Eduardo Cristerna <eduardo@schoolhub.com>
2016-09-08 21:29:10 -0500

    textfields navigation

--- ios/Focus/Log In/RegistrationViewController.swift
+++ ios/Focus/Log In/RegistrationViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class RegistrationViewController: UIViewController {
     
     @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var contentView: UIView!
     
     @IBOutlet var usernameText: UITextField!
     @IBOutlet var passwordText: UITextField!
@@ -24,11 +25,64 @@ class RegistrationViewController: UIViewController {
     @IBOutlet var maleButton: RadioButton!
     @IBOutlet var femaleButton: RadioButton!
     
+    @IBOutlet var streetText: UITextField!
+    @IBOutlet var numberText: UITextField!
+    @IBOutlet var coloniaText: UITextField!
+    @IBOutlet var cityText: UITextField!
+    @IBOutlet var stateText: UITextField!
+    @IBOutlet var postalText: UITextField!
+    
+    var activeField: UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardDidShow(_:)), name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.keyboardWillBeHidden(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
+    
+    // -----------------------------------------------------------------------------------------------------------
+    // MARK: - Keyboard
+    // -----------------------------------------------------------------------------------------------------------
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.activeField = nil
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        self.activeField = textField
+    }
+    
+    func keyboardDidShow(notification: NSNotification) {
+        if let activeField = self.activeField, keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            self.scrollView.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height + 10, right: 0.0)
+            self.scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height + 10, right: 0.0)
+            var aRect = self.view.frame
+            aRect.size.height -= keyboardSize.size.height
+            
+            if (!CGRectContainsPoint(aRect, activeField.frame.origin)) {
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        self.scrollView.contentInset = UIEdgeInsetsZero
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsetsZero
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        (self.contentView.viewWithTag(textField.tag + 10) as! UITextField).becomeFirstResponder()
+        return false
+    }
+    
+    
     
     @IBAction func register(sender: AnyObject) {
         let user = User(id: 1, username: "Carlos", email: "carlosmay@hotmail.com", nombre: "Carlos Mayo Rodríguez", genero: 0)
