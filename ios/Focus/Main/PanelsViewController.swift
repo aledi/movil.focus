32d64d1bbf01f0590df48faae06621503a2e024a
Eduardo Cristerna <eduardo@schoolhub.com>
2016-12-31 19:13:54 -0600

    accept/reject

--- ios/Focus/Main/PanelsViewController.swift
+++ ios/Focus/Main/PanelsViewController.swift
@@ -12,6 +12,9 @@ class PanelsViewController: UITableViewController {
     
     var paneles: [Panel]?
     var selectedPanel: Panel?
+    var selectedIndex = 0
+    var selectedAction = 0
+    var loadingAlert: UIAlertController?
     
     // -----------------------------------------------------------------------------------------------------------
     // MARK: - Lifecycle
@@ -72,15 +75,90 @@ class PanelsViewController: UITableViewController {
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         let panel = self.paneles![indexPath.row]
+        
         self.selectedPanel = panel
+        self.selectedIndex = indexPath.row
         
         if (panel.estado != .Accepted) {
-            self.presentAlertWithTitle("Invitación", withMessage: "\(panel.descripcion)\n\nAcepte la invitación para comenzar a participar.", withButtonTitles: ["Aceptar", "Rechazar", "Cancelar"], withButtonStyles: [.Default, .Destructive, .Cancel], andButtonHandlers: [nil, nil, nil])
+            self.presentAlertWithTitle("Invitación", withMessage: "\(panel.descripcion)\n\nAcepte la invitación para comenzar a participar.", withButtonTitles: ["Aceptar", "Rechazar", "Cancelar"], withButtonStyles: [.Default, .Destructive, .Cancel], andButtonHandlers: [self.acceptInvitation, self.rejectInvitation, nil])
         } else if (panel.encuestas?.count == 0) {
             self.presentAlertWithTitle("No hay Encuestas", withMessage: "Este panel aún no cuenta con encuestas. Por favor, espere a que una encuesta sea habilitada.", withButtonTitles: ["OK"], withButtonStyles: [.Cancel], andButtonHandlers: [nil])
         } else {
             self.performSegueWithIdentifier("showEncuestas", sender: panel)
         }
+    }
+    
+    // -----------------------------------------------------------------------------------------------------------
+    // MARK: - Accept/Reject
+    // -----------------------------------------------------------------------------------------------------------
+    
+    func acceptInvitation(action: UIAlertAction) {
+        guard let panelistaId = User.currentUser?.id, let panelId = self.selectedPanel?.id else {
+            return
+        }
+        
+        self.selectedAction = 1
+        self.acceptRejectCall(panelistaId, panelId: panelId, status: EstadoPanel.Accepted.rawValue)
+    }
+    
+    func rejectInvitation(action: UIAlertAction) {
+        guard let panelistaId = User.currentUser?.id, let panelId = self.selectedPanel?.id else {
+            return
+        }
+        
+        self.selectedAction = 2
+        self.acceptRejectCall(panelistaId, panelId: panelId, status: EstadoPanel.Rejected.rawValue)
+    }
+    
+    func acceptRejectCall(panelistaId: Int, panelId: Int, status: Int) {
+        let parameters: [String : AnyObject] = [
+            "panelista" : panelistaId,
+            "panel" : panelId,
+            "estado" : status
+        ]
+        
+        self.loadingAlert = self.presentAlertWithTitle("Cargando", withMessage: nil, withButtonTitles: [], withButtonStyles: [], andButtonHandlers: [])
+        Controller.requestForAction(.INVITATION_RESPONE, withParameters: parameters, withSuccessHandler: self.successHandler, andErrorHandler: self.errorHandler)
+    }
+    
+    func successHandler(response: NSDictionary) {
+        self.loadingAlert?.dismissViewControllerAnimated(false, completion: {
+            if (response["status"] as? String == "SUCCESS") {
+                if (self.selectedAction == 1) {
+                    self.selectedPanel?.estado = .Accepted
+                } else {
+                    self.selectedPanel?.estado = .Rejected
+                    self.paneles?.removeAtIndex(self.selectedIndex)
+                }
+                
+                self.selectedAction = 0
+                self.selectedIndex = 0
+                self.selectedPanel = nil
+                
+                self.tableView.reloadData()
+            } else {
+                self.presentAlertWithTitle("Error", withMessage: "Hubo un error al procesar su respuesta. Por favor, inténtelo más tarde.", withButtonTitles: ["OK"], withButtonStyles: [.Cancel], andButtonHandlers: [nil])
+            }
+        })
+    }
+    
+    func errorHandler(response: NSDictionary) {
+        self.loadingAlert?.dismissViewControllerAnimated(false, completion: {
+            var alertTitle = ""
+            var alertMessage = ""
+            
+            switch (response["error"] as! NSError).code {
+            case -1009:
+                alertTitle = "Sin conexión a internet"
+                alertMessage = "Para utilizar la aplicación, su dispositivo debe estar conectado a internet."
+            default:
+                alertTitle = "Servidor no disponible"
+                alertMessage = "Nuestro servidor no está disponible por el momento."
+            }
+            
+            self.presentAlertWithTitle(alertTitle, withMessage: alertMessage, withButtonTitles: ["OK"], withButtonStyles: [.Cancel], andButtonHandlers: [nil])
+            print(response["error"])
+        })
     }
     
 }
